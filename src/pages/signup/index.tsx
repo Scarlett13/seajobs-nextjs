74ba89a0d23ba2d95c015111f9e976055f7903ba
ctscope-development <development@ctscope.io>
2023-02-16 12:38:35 +0700

    feat/signup with amplify done

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import { signupFields } from "../../constants/authformconstants/AuthFormConstants";
 import Input from "../../components/inputs/reguler/InputTemplate";
 import { NextPageWithLayout } from "../page";
@@ -9,8 +9,10 @@ import PasswordTemplate from "../../components/inputs/password/PasswordTemplate"
 import usePush from "@utils/UsePush";
 import { useUser } from "../../contexts/AmplifyAuthContext";
 import { Alert, Snackbar, SnackbarCloseReason } from "@mui/material";
+import { signup } from "@utils/AuthUtils";
+import { isEmpty } from "@utils/StringUtils";
 
-const SignupForm: NextPageWithLayout = () => {
+export default function Signup() {
   const fields = signupFields;
   let fieldsState: any = {};
 
@@ -22,12 +24,14 @@ const SignupForm: NextPageWithLayout = () => {
 
   const [open, setOpen] = useState(false);
   const [signUpError, setSignUpError] = useState<string>("");
+  const [signUpSuccess, setSignUpSuccess] = useState<string>("");
 
-  const { user, authenticated } = useUser();
+  const { user, setUser, authenticated, setAuthenticated } = useUser();
 
   useEffect(() => {
     console.log("user effect login: ", user);
     console.log("auth effect login: ", authenticated);
+
     if (authenticated) {
       push("/dashboard");
     }
@@ -53,122 +57,122 @@ const SignupForm: NextPageWithLayout = () => {
     setSignupState({ ...signupState, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = (e: { preventDefault: () => void }) => {
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     if (passwordMatch) {
       console.log(signupState);
-      createAccount();
+      const retdata = await signup(
+        signupState.fullname,
+        signupState.email_address,
+        signupState.password
+      );
+      if (retdata.success) {
+        setSignUpError("");
+        setSignUpSuccess(
+          "Email konfirmasi telah dikirim ke " +
+            signupState.email_address +
+            ", silahkan cek email anda!"
+        );
+        setOpen(true);
+        // push("/login");
+      } else {
+        setSignUpError(retdata.data.message);
+        setSignUpSuccess("");
+        setOpen(true);
+      }
     }
   };
 
-  //handle Signup API Integration here
-  const createAccount = () => {
-    console.log(signupState);
-  };
-
-  const handleClose = (
-    event: React.SyntheticEvent<any, Event>,
-    reason: SnackbarCloseReason
+  const handleSnackBarClose = (
+    event: Event | SyntheticEvent<any, Event>,
+    reason: SnackbarCloseReason | null
   ) => {
     if (reason === "clickaway") {
       return;
     }
+    setSignUpError("");
+    setSignUpSuccess("");
     setOpen(false);
   };
 
-  return (
-    <>
-      <form className="mt-10" onSubmit={handleSubmit} autoComplete="off">
-        <div className="">
-          {fields.map((field) =>
-            field.type === "password" ? (
-              <section key={field.titelKey}>
-                <p className="-mb-4 font-light text-gray-400">
-                  {field.labelText}
-                </p>
-                <PasswordTemplate
-                  key={field.id}
-                  handleChange={handleChange}
-                  value={signupState[field.id]}
-                  labelText={field.labelText}
-                  labelFor={field.labelFor}
-                  id={field.id}
-                  name={field.name}
-                  type={field.type}
-                  isRequired={field.isRequired}
-                  placeholder={field.placeholder}
-                />
-              </section>
-            ) : (
-              <section key={field.titelKey}>
-                <p className="-mb-4 font-light text-gray-400">
-                  {field.labelText}
-                </p>
-                <Input
-                  key={field.id}
-                  handleChange={handleChange}
-                  value={signupState[field.id]}
-                  labelText={field.labelText}
-                  labelFor={field.labelFor}
-                  id={field.id}
-                  name={field.name}
-                  type={field.type}
-                  isRequired={field.isRequired}
-                  placeholder={field.placeholder}
-                />
-              </section>
-            )
-          )}
-          <p className={"-mt-4 font-extra-light text-sm text-red-500"}>
-            {passwordMatch ? "" : "password tidak cocok"}
-          </p>
-          <FormAction handleSubmit={handleSubmit} text="Signup" />
-        </div>
-      </form>
-      <Snackbar
-        open={open}
-        autoHideDuration={6000}
-        onClose={() => {
-          handleClose;
-        }}
-      >
-        <Alert
-          onClose={() => {
-            handleClose;
-          }}
-          severity="error"
-        >
-          {signUpError}
-        </Alert>
-      </Snackbar>
-    </>
-  );
-};
+  const handleAlertClose = () => {
+    setOpen(false);
+    setSignUpError("");
+    setSignUpSuccess("");
+  };
 
-function Signup() {
   return (
-    <>
-      <section>
-        <FormHeaderLayout
-          heading="Daftar"
-          paragraph="Sudah mempunyai akun? "
-          linkName="Masuk"
-          linkUrl="/login"
+    <PrimaryLayout user={user}>
+      <FormHeaderLayout
+        heading="Daftar"
+        paragraph="Sudah mempunyai akun? "
+        linkName="Masuk"
+        linkUrl="/login"
+      >
+        <form className="mt-10" onSubmit={handleSubmit} autoComplete="off">
+          <div className="">
+            {fields.map((field) =>
+              field.type === "password" ? (
+                <section key={field.titelKey}>
+                  <p className="-mb-4 font-light text-gray-400">
+                    {field.labelText}
+                  </p>
+                  <PasswordTemplate
+                    key={field.id}
+                    handleChange={handleChange}
+                    value={signupState[field.id]}
+                    labelText={field.labelText}
+                    labelFor={field.labelFor}
+                    id={field.id}
+                    name={field.name}
+                    type={field.type}
+                    isRequired={field.isRequired}
+                    placeholder={field.placeholder}
+                  />
+                </section>
+              ) : (
+                <section key={field.titelKey}>
+                  <p className="-mb-4 font-light text-gray-400">
+                    {field.labelText}
+                  </p>
+                  <Input
+                    key={field.id}
+                    handleChange={handleChange}
+                    value={signupState[field.id]}
+                    labelText={field.labelText}
+                    labelFor={field.labelFor}
+                    id={field.id}
+                    name={field.name}
+                    type={field.type}
+                    isRequired={field.isRequired}
+                    placeholder={field.placeholder}
+                  />
+                </section>
+              )
+            )}
+            <p className={"-mt-4 font-extra-light text-sm text-red-500"}>
+              {passwordMatch ? "" : "password tidak cocok"}
+            </p>
+            <FormAction handleSubmit={handleSubmit} text="Signup" />
+          </div>
+        </form>
+        <Snackbar
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleSnackBarClose}
         >
-          <SignupForm />
-        </FormHeaderLayout>
-      </section>
-    </>
+          <Alert
+            onClose={handleAlertClose}
+            severity={isEmpty(signUpError) ? "success" : "error"}
+          >
+            {isEmpty(signUpError) ? signUpSuccess : signUpError}
+          </Alert>
+        </Snackbar>
+      </FormHeaderLayout>
+    </PrimaryLayout>
   );
 }
 
-export default Signup;
+// export default Signup;
 
 Signup.authenticate = false;
-Signup.getLayout = (page: any, authenticated: boolean) => {
-  return (
-    <>
-      <PrimaryLayout>{page}</PrimaryLayout>
-    </>
-  );
-};
