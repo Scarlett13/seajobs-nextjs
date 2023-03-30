fc545641f80c9327108254aae26493c050dbf74a
ctscope-development <development@ctscope.io>
2023-03-30 14:22:36 +0700

    fix: error mocking data in ProjectCard component fixed

--- src/page_components/explore/cards/ProjectCard.mocks.ts
+++ src/page_components/explore/cards/ProjectCard.mocks.ts
@@ -1,7 +1,25 @@
+import { IAmplifyProjectCard } from '../../../constants/exploreformconstants/ProjectCard';
 import { IProjectCard } from './ProjectCard';
 
+const dummyProject: IAmplifyProjectCard = 
+  {
+    projectId: "pid",
+    projectTitle: "Project Perusahaan Dummy",
+    projectLocation: "Jakarta",
+    projectValue: "500,000,000",
+    projectDuration: "6 Bulan",
+    projectStart: "28 Oktober 2023",
+    projectCategories: "Finance",
+    projectDescription: "Disini project description yang panjang",
+    projectClient: "Pemprov DKI",
+    projectDeadline: "Sebelum 20 September 2023",
+    projectOwner: "PT Adhikari",
+    isActive: "true",
+    projectStatus: "Bidding",
+    isDeleted: false,
+  }
 const base: IProjectCard = {
-  sampleTextProp: 'Hello world!',
+  project: dummyProject,
 };
 
 export const mockProjectCardProps = {
